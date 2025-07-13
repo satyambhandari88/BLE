896d82b6e7e0975f482d16f909e1ad74996bb491
satyambhandari88 <satyambhandari7247@gmail.com>
2025-07-14 03:52:06 +0530

    Delete backend directory

--- backend/routes/teacherRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createClass } = require('../controllers/teacherController');
-const router = express.Router();
-
-// Teacher Create Class Route
-router.post('/create-class', createClass);
-
-module.exports = router;
